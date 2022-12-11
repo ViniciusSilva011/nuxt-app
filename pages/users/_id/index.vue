ade3ccb06bcb766640dcbbc542025dfa8f7ecc61
Vinicius Silva <vinicius.silva.t0114629@edu.atec.pt>
2022-12-11 13:48:55 +0000

    add simple routes and validation

---
+++ pages/users/_id/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    User: {{this.$route.params.id}}
+  </div>
+</template>
+
+<script>
+export default {
+  validate(context) {
+    return (/\d+$/g).test(context.params.id);
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/users/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    User id:
+    <v-text-field v-model="id"></v-text-field>
+    <v-btn @click="loadUser">
+      goto
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      id: ''
+    }
+  },
+  methods:{
+    loadUser() {
+      this.$router.push(`/users/${this.id}`)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
